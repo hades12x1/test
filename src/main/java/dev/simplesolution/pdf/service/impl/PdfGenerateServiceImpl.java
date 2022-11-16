50a8561c1c43f1d9ed12f2f9017c87e801db92d0
chuyenns <kedoitao1998>
2022-11-16 21:05:54 +0700

    temp

---
+++ src/main/java/dev/simplesolution/pdf/service/impl/PdfGenerateServiceImpl.java
@@ -0,0 +1,36 @@
+package dev.simplesolution.pdf.service.impl;
+
+import dev.simplesolution.pdf.service.PdfGenerateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.FileOutputStream;
+import java.util.Map;
+
+@Service
+public class PdfGenerateServiceImpl implements PdfGenerateService {
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Override
+    public void generatePdfFile(String templateName, Map<String, Object> data, String pdfFileName) {
+        Context context = new Context();
+        context.setVariables(data);
+
+        String htmlContent = templateEngine.process(templateName, context);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(pdfFileName);
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocumentFromString(htmlContent);
+            renderer.layout();
+            renderer.createPDF(fileOutputStream, false);
+            renderer.finishPDF();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
